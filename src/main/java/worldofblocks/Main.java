b3b125cfc4259288a5294117b5e85d5af1d030bb
simplay <silent.simplay@gmail.com>
2019-12-15 22:14:33 +0100

    Log some basic system information

--- src/main/java/worldofblocks/Main.java
+++ src/main/java/worldofblocks/Main.java
@@ -1,11 +1,17 @@
 package worldofblocks;
 
 import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
 import worldofblocks.game.Game;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Using LWJGL " + Version.getVersion());
+    System.out.println("System Information:");
+    System.out.println("OS: " + System.getProperty("os.name"));
+    System.out.println("JAVA " + System.getProperty("java.version"));
+    System.out.println("LWJGL " + Version.getVersion());
+    System.out.println("GLFW " + GLFW.glfwGetVersionString());
+
     new Game(1024, 768, false).start();
   }
 }
